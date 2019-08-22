72bdc9a712fd2a56b9de05c29a3419c10838dc0f
Abraham <cedenoabraham@gmail.com>
2019-08-22 00:35:59 -0300

    lazy load

--- client/App.js
+++ client/App.js
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, { lazy, Suspense }  from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import history from './history';
 
 import './App.scss';
-import Login from './components/Login';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Side from './components/Side';
-import Chat from './components/Chat';
-import Signup from "./components/Signup";
+const Login = lazy( () => import('./components/Login'));
+const Header = lazy( () => import('./components/Header'));
+const Footer  = lazy( () => import('./components/Footer'));
+const Side = lazy( () => import('./components/Side'));
+const Chat = lazy( () => import('./components/Chat'));
+const Signup = lazy ( () => import('./components/Signup'));
 
 import { ChitChatProvider } from "./context/ChitChatContext";
 
+const renderLoader = () => <p>Loading</p>;
 
 export default function(){
     return (
         <ChitChatProvider>
             <div className="poligon"></div>
             <div className="container">
-                <Header />
-                <Side />
-                <Router history={history}>
-                    <Switch>
-                        <Route exact path="/" component={ Login } />
-                        <Route exact path="/signup" component={ Signup } />
-                        <Route path="/chat" exact component={ Chat } />
-                    </Switch>
-                </Router>
-                <Footer />
+                <Suspense fallback={renderLoader()}>
+                    <Header />
+                    <Side />
+                    <Router history={history}>
+                        <Switch>
+                            <Route exact path="/" component={ Login } />
+                            <Route exact path="/signup" component={ Signup } />
+                            <Route path="/chat" exact component={ Chat } />
+                        </Switch>
+                    </Router>
+                    <Footer />
+                </Suspense>
             </div>
         </ChitChatProvider>
     )

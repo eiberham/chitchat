bea6ad0bae89068bccdc0a368ef5e2d96ab9e423
Abraham <asap_acedeno@lnexternos.com.ar>
2020-02-03 11:56:22 -0300

    dependencies update along with login unit test

--- client/components/Login/self.test.js
+++ client/components/Login/self.test.js
@@ -1,10 +1,19 @@
+import 'regenerator-runtime/runtime';
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from 'enzyme';
+import { MockedProvider } from "@apollo/react-testing";
 import Login from './index';
 
 describe('Login', () => {
     it('Matches snapshot', () => {
-        const component = render(<Login />);
+        const component = render(
+            <MockedProvider>
+                <Router>
+                    <Login />
+                </Router>
+            </MockedProvider>
+        );
         expect(component).toMatchSnapshot();
     })
 });
--- client/components/Side/index.js
+++ client/components/Side/index.js
@@ -7,7 +7,7 @@ export default function(){
             <h1>You can:</h1>
             <ul>
                 <li>Have a nice convo</li>
-                <li>Meet people who shares your same interests</li>
+                <li>Meet people who share your same interests</li>
                 <li>Enhance your english skills</li>
                 <li>Make new friends</li>
             </ul>

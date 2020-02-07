52ec2ec43dec630f19911fd82369de47576fdbec
Abraham <cedenoabraham@gmail.com>
2020-02-07 00:48:22 -0300

    some e2e test

--- client/cypress/integration/login_spec.js
+++ client/cypress/integration/login_spec.js
@@ -1,9 +1,26 @@
 describe('Login', function() {
-    it('successfully loads', function() {
+    it('Successfully loads', function() {
       cy.visit('/')
+    });
+
+    it('Redirects to sign up page wonderfully', function(){
+      cy.get('.sign-up__link > a').click();
+      cy.location().should((loc) => {
+        expect(loc.pathname).to.eq('/signup')
+      })
     })
 
-    it('Does not do much!', function() {
+    it('Logs in like a champ', function() {
+      cy.visit('/')
+      cy.get('#email').type('cedenoabraham@gmail.com')
+      cy.get('#password').type('acedeno')
+      cy.get('button').click()
+      cy.location().should((loc) => {
+        expect(loc.pathname).to.eq('/chat')
+      })
+    });
+
+    it('Ok this one is useless', function() {
       expect(true).to.equal(true)
     })
 })

6ac703cf4163d3164734bfe7e6067b07b0c4b708
Abraham <cedenoabraham@gmail.com>
2020-02-06 23:57:38 -0300

    cypressgit add .

--- client/cypress/integration/login_spec.js
+++ client/cypress/integration/login_spec.js
@@ -1,4 +1,8 @@
-describe('My First Test', function() {
+describe('Login', function() {
+    it('successfully loads', function() {
+      cy.visit('/')
+    })
+
     it('Does not do much!', function() {
       expect(true).to.equal(true)
     })
